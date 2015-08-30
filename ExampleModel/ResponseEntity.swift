5cc7fc7c0f9b9525b7bce6ff7b2f85a232a2cbad
Yoichi Tagaya <yoichi@el-eleven.com>
2015-08-30 15:51:50 +0900

    Add ImageEntity, ResponseEntity and their tests.

---
+++ ExampleModel/ResponseEntity.swift
@@ -0,0 +1,24 @@
+//
+//  ResponseEntity.swift
+//  SwinjectMVVMExample
+//
+//  Created by Yoichi Tagaya on 8/29/15.
+//  Copyright © 2015 Swinject Contributors. All rights reserved.
+//
+
+import Himotoki
+
+public struct ResponseEntity {
+    public let totalCount: Int64
+    public let images: [ImageEntity]
+}
+
+// MARK: Decodable
+extension ResponseEntity: Decodable {
+    public static func decode(e: Extractor) -> ResponseEntity? {
+        return build(ResponseEntity.init)(
+            e <| "totalHits",
+            e <|| "hits"
+        )
+    }
+}
