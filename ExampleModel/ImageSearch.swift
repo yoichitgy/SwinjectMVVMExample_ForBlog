cf248b573b5cd47db1ae8664013675550a886601
Yoichi Tagaya <yoichi@el-eleven.com>
2015-08-30 20:20:05 +0900

    Add ImageSearching, ImageSearch, Pixabay and ImageSearchSpec.

---
+++ ExampleModel/ImageSearch.swift
@@ -0,0 +1,33 @@
+//
+//  ImageSearch.swift
+//  SwinjectMVVMExample
+//
+//  Created by Yoichi Tagaya on 8/30/15.
+//  Copyright © 2015 Swinject Contributors. All rights reserved.
+//
+
+import ReactiveCocoa
+import Result
+import Himotoki
+
+public final class ImageSearch: ImageSearching {
+    private let network: Networking
+    
+    public init(network: Networking) {
+        self.network = network
+    }
+    
+    public func searchImages() -> SignalProducer<ResponseEntity, NetworkError> {
+        let url = Pixabay.apiURL
+        let parameters = Pixabay.requestParameters
+        return network.requestJSON(url, parameters: parameters)
+            .attemptMap { json in
+                if let response = decode(json) as ResponseEntity? {
+                    return Result(value: response)
+                }
+                else {
+                    return Result(error: .IncorrectDataReturned)
+                }
+        }
+    }
+}
