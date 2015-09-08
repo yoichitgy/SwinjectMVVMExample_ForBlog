fcf6dda4024e360cead1992362b27075400fc040
Yoichi Tagaya <yoichi@el-eleven.com>
2015-09-08 18:23:41 +0900

    Add getPreviewImage method to ImageSearchTableViewCellModeling protocol and its implementation class.

--- ExampleViewModel/ImageSearchTableViewCellModel.swift
+++ ExampleViewModel/ImageSearchTableViewCellModel.swift
@@ -6,16 +6,45 @@
 //  Copyright © 2015 Swinject Contributors. All rights reserved.
 //
 
+import ReactiveCocoa
 import ExampleModel
 
-public final class ImageSearchTableViewCellModel: ImageSearchTableViewCellModeling {
+public final class ImageSearchTableViewCellModel
+    : NSObject, ImageSearchTableViewCellModeling
+{
     public let id: UInt64
     public let pageImageSizeText: String
     public let tagText: String
+    
+    private let network: Networking
+    private let previewURL: String
+    private var previewImage: UIImage?
 
-    internal init(image: ImageEntity) {
+    internal init(image: ImageEntity, network: Networking) {
         id = image.id
         pageImageSizeText = "\(image.pageImageWidth) x \(image.pageImageHeight)"
         tagText = image.tags.joinWithSeparator(", ")
+        
+        self.network = network
+        previewURL = image.previewURL
+        
+        super.init()
+    }
+    
+    public func getPreviewImage() -> SignalProducer<UIImage?, NoError> {
+        if let previewImage = self.previewImage {
+            return SignalProducer(value: previewImage).observeOn(UIScheduler())
+        }
+        else {
+            let imageProducer = network.requestImage(previewURL)
+                .takeUntil(self.racutil_willDeallocProducer)
+                .on(next: { self.previewImage = $0 })
+                .map { $0 as UIImage? }
+                .flatMapError { _ in SignalProducer<UIImage?, NoError>(value: nil) }
+            
+            return SignalProducer(value: nil)
+                .concat(imageProducer)
+                .observeOn(UIScheduler())
+        }
     }
 }
